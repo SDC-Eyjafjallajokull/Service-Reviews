a5d023fa88c3cd88deb7fb911d22e26f2d7ef10a
Kevin (Yiming) Chen <kevinchenyiming@gmail.com>
2020-12-02 00:22:40 -0800

    implemented CRUD

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const server = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
-const port = 3000;
-const Product = require('../database/index.js');
 const path = require('path');
+const Product = require('../database/index.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+const server = express();
 
-
+const port = 3000;
+server.listen(port, () => {
+  console.log('connected to server and listening on port 3000');
+});
+server.use(express.static(path.join(__dirname, '../client/dist')));
 server.use(bodyparser.json());
 server.use(bodyparser.urlencoded({extended: true}));
 server.use(cors());
 server.use(morgan('dev'));
+
+mongoose.connect('mongodb://localhost/reviews', { useNewUrlParser: true, useUnifiedTopology: true } )
+.then(() => {
+  console.log('connected to mongo');
+})
+.catch((err) => {
+  console.error(err);
+})
 
 server.get('/api/products', (req, res) => {
   Product.find({})
@@ -34,15 +47,24 @@ server.put('/api/products/:id/review', (req, res) => {
   .catch((err) => {
     res.status(400).send(err);
   });
-})
-
-server.use(express.static(path.join(__dirname, '../client/dist')));
-
-
-
-
-
-server.listen(port, () => {
-  console.log('connected to server and listening on port 3000');
 });
 
+server.delete('/api/products/:id', (req, res) => {
+  Product.findOneAndDelete({ "_id": req.params.id })
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+server.post('/api/products', (req, res) => {
+  Product.create(req.body.product)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
